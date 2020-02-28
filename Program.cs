74523fda5d52d789f0f078163d671457e28606ef
Sang291000 <61574195+Sang291000@users.noreply.github.com>
2020-02-28 16:06:51 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagers
+{
+    class Program
+    {
+        public static List<SinhVien> List = new List<SinhVien>();
+        static void Main(string[] args)
+        {
+            menu();
+        }
+        public static void menu()
+        {
+            Console.WriteLine("1.Nhap sinh vien ");
+            Console.WriteLine("2.Xuat sinh vien ");
+            Console.WriteLine("3.Tim Kiem sinh vien ");
+            Console.WriteLine("4.Xoa sinh vien ");
+            Console.WriteLine("5.Sua sinh vien ");
+            Console.WriteLine("6.thoat ");
+            Console.Write("Bam Chon :");
+            int luachon = int.Parse(Console.ReadLine());
+            switch (luachon)
+            {
+                case 1: nhapds(); menu(); break;
+                case 2: xuatds(); menu(); break;
+                case 3: timkiem(); menu(); break;
+                case 4: xoa(); menu(); break;
+                case 5: sua(); menu(); break;
+                case 6: break;
+            }
+        }
+        public static void nhapds()
+        {
+            Console.WriteLine("nhap vao so sinh vien :");
+            int sosv = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sosv; i++)
+            {
+                SinhVien sv = new SinhVien();
+                sv.nhap();
+                List.Add(sv);
+            }
+        }
+        public static void xuatds()
+        {
+            SinhVien.xuatTD();
+            for (int i = 0; i < List.Count; i++)
+            {
+                List[i].xuat();
+
+            }
+        }
+        public static void timkiem()
+        {
+            Console.Write("Nhap vao ma:");
+            string ma = Console.ReadLine();
+            SinhVien.xuatTD();
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].Masv == ma)
+                {
+                    List[i].xuat();
+                }
+
+            }
+        }
+        public static void xoa()
+        {
+            Console.Write("Nhap vao ma:");
+            string ma = Console.ReadLine();
+            SinhVien.xuatTD();
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].Masv == ma)
+                {
+                    List.RemoveAt(i);
+                }
+
+            }
+            xuatds();
+
+        }
+        public static void sua()
+        {
+            Console.Write("Nhap vao ma:");
+            string ma = Console.ReadLine();
+            SinhVien.xuatTD();
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i].Masv == ma)
+                {
+                    List[i].nhap();
+                }
+
+            }
+            SinhVien.xuatTD();
+            xuatds();
+
+        }
+
+    }
+}
---
+++ SinhVien.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagers
+{
+    class SinhVien
+    {
+        private string masv;
+        private string tensv;
+        private int Tuoi;
+        private string Email;
+        private string Quequan;
+
+
+        public string Masv
+        {
+            get
+            {
+                return masv;
+            }
+            set
+            {
+                masv = value;
+            }
+        }
+        public string Tensv
+        {
+            get
+            {
+                return tensv;
+            }
+            set
+            {
+                tensv = value;
+            }
+
+        }
+        public string tuoi
+        {
+            get
+            {
+                return tuoi;
+            }
+            set
+            {
+                tuoi = value;
+            }
+        }
+        public string email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string quequan
+        {
+            get
+            {
+                return quequan;
+            }
+            set
+            {
+                quequan = value;
+            }
+        }
+        public SinhVien() { }
+        public SinhVien(string masv, string tensv, int tuoi,string email, string quequan)
+        {
+            this.masv = masv;
+            this.Tensv = tensv;
+            this.Tuoi = tuoi;
+            this.Email = email;
+            this.Quequan = quequan;
+
+        }
+        public void nhap()
+        {
+            Console.Write("Nhap masv :");
+            this.Masv = Console.ReadLine();
+            Console.Write("Nhap tensv :");
+            this.Tensv = Console.ReadLine();
+            Console.Write("Nhap tuoi :");
+            this.Tuoi = int.Parse(Console.ReadLine());
+            Console.Write("Nhap email :");
+            this.Email = Console.ReadLine();
+            Console.Write("Nhap quequan :");
+            this.Quequan = Console.ReadLine();
+        }
+        public void xuat()
+        {
+            string chuoi = string.Format("{0,15}{1,15}{2,15}{3,15}{4,15}", this.Masv, this.Tensv, this.Tuoi, this.Email, this.Quequan);
+            Console.WriteLine(chuoi);
+
+        }
+        public static void xuatTD()
+        {
+            string chuoi = string.Format("{0,15}{1,15}{2,15}{3,15}{4,15}", "Ma Sinh Vien", "Ten Sinh Vien", "Tuoi", "Email", "Que Quan");
+            Console.WriteLine(chuoi);
+
+        }
+    }
+}  
